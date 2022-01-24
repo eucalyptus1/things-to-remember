b98b87d223c89f9017a180a0186d3c345d422c04
euc1 <jamesharrington585@hotmail.com>
2022-01-23 20:59:13 -0500

    added weather api to js and html. implemented fine, but style and position need adjusting

--- assets/java/script.js
+++ assets/java/script.js
@@ -11,6 +11,73 @@ const backDrop = document.getElementById('modalBackDrop');
 const eventTitleInput = document.getElementById('eventTitleInput');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+
+//weather API
+
+var iconElement = document.querySelector(".icon");
+var tempElement = document.querySelector(".temp p");
+var descElement = document.querySelector(".desc p");
+var locationElement = document.querySelector(".city p");
+
+var key = "0362bdb6062e171586847be4bbcbb408";
+
+const weather = {};
+
+//convert temperature to celcius
+var KELVIN = 273;
+
+weather.temperature = {
+    unit : "celsius"
+}
+
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+
+
+
+//geolocation for getting users location
+navigator.geolocation.getCurrentPosition(setPosition);
+
+
+function setPosition(position){
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    
+    getWeather(lat, lon);
+}
+
+//call for openweather api
+function getWeather(lat, lon){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+// display function
+function displayWeather(){
+    iconElement.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.iconId}@4x.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}`;
+}
+//end weather api
+
 //clicking date function
 function openModal(date) {
   clicked = date;
@@ -33,16 +100,16 @@ function load() {
   if (nav !== 0) {
     dt.setMonth(new Date().getMonth() + nav);
   }
-// these 3 lines are geting the actual day,year&month 
+  // these 3 lines are geting the actual day,year&month 
   const day = dt.getDate();
   const month = dt.getMonth();
   const year = dt.getFullYear();
-//Getting the day of the month the code was created, the date string is 
+  //Getting the day of the month the code was created, the date string is 
   const firstDayOfMonth = new Date(year, month, 1);
   const daysInMonth = new Date(year, month + 1, 0).getDate();
   //US calendar 
   const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
-      //passing information that we want, numrc is giving us the actual number of the date, rather then telling us in letters
+    //passing information that we want, numrc is giving us the actual number of the date, rather then telling us in letters
     weekday: 'long',
     year: 'numeric',
     month: 'numeric',
@@ -51,17 +118,17 @@ function load() {
   //varuable 
   const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
 
-  document.getElementById('monthDisplay').innerText = 
+  document.getElementById('monthDisplay').innerText =
     `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
-// continuing to get us the next month
+  // continuing to get us the next month
   calendar.innerHTML = '';
 
-  for(let i = 1; i <= paddingDays + daysInMonth; i++) {
+  for (let i = 1; i <= paddingDays + daysInMonth; i++) {
     const daySquare = document.createElement('div');
     daySquare.classList.add('day');
 
     const dayString = `${month + 1}/${i - paddingDays}/${year}`;
-// giving the actual number of the day we are on
+    // giving the actual number of the day we are on
     if (i > paddingDays) {
       daySquare.innerText = i - paddingDays;
       const eventForDay = events.find(e => e.date === dayString);
@@ -82,7 +149,7 @@ function load() {
       daySquare.classList.add('padding');
     }
 
-    calendar.appendChild(daySquare);    
+    calendar.appendChild(daySquare);
   }
 }
 
@@ -128,17 +195,16 @@ function initButtons() {
     nav--;
     load();
   });
-// event listener for task edits
+  // event listener for task edits
   document.getElementById('saveButton').addEventListener('click', saveEvent);
   document.getElementById('cancelButton').addEventListener('click', closeModal);
   document.getElementById('deleteButton').addEventListener('click', deleteEvent);
   document.getElementById('closeButton').addEventListener('click', closeModal);
 }
 
+
 initButtons();
 load();
 
 console.dir(window.document);
-
-
 
